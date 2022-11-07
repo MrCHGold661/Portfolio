12d17d551115cd87c1341acd069bfcaa4bd9146f
Calvin Goldman <2026goldman.calvin@fcboe.org>
2022-11-07 14:48:38 -0500

    new c name program

---
+++ C/integers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	int x;
+        x = 2 + 4;
+	printf("%i\n", "1: ", x);
+	x = 6 * 6;
+	printf("%i\n", "2: ", x);
+        x = 4 * 4 - 2;
+	printf("%i\n", "3: ", x);
+        x = 12 / 3 - 20;
+	printf("%i\n", "4: ", x);
+        x = 25 / 4;
+	printf("%i\n", "5: ", x);
+	return 0;
+}
---
+++ C/name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	char balls[30];
+	printf("What is your name? ");
+	scanf("%s" , &balls);
+	printf("your name is %s\n", balls, "Ok?");
+	return 0;
+
+}
